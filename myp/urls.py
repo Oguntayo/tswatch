29dff2cc524e03b085e5b85114d16d3ff0119281
oguntayo <90482951+Oguntayo@users.noreply.github.com>
2022-03-14 08:12:44 +1200

    Update urls.py

--- myp/urls.py
+++ myp/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stopwatch/', include("stopwatch.urls"))
+    path('stopwatch/', include("stopwatch.urls")),
+    
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
